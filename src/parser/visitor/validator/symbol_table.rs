f7f7cf03e4e17eb4a2cce89a3ed89ad210f7159f
Scott Godwin <sgodwincs@gmail.com>
2017-07-13 15:15:23 -0400

    add error handling for symbol table

--- src/parser/visitor/validator/symbol_table.rs
+++ src/parser/visitor/validator/symbol_table.rs
@@ -6,6 +6,30 @@ use std::collections::HashMap;
 
 use parser::ast::Identifier;
 
+pub mod errors {
+    use super::Symbol;
+
+    error_chain! {
+        errors {
+            CannotExitGlobalScope {
+                description("cannot exit global scope")
+            }
+            ScopeAlreadyExists(scope: String) {
+                description("scope already exists")
+                display("scope already exists: `{}`", scope)
+            }
+            ScopeDoesNotExist(scope: String) {
+                description("scope does not exist")
+                display("scope does not exist: `{}`", scope)
+            }
+            SymbolAlreadyExists(symbol: Symbol) {
+                description("symbol already exists")
+                display("symbol already exists: `{:?}`", symbol)
+            }
+        }
+    }
+}
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Symbol {
     pub name: Identifier,
@@ -58,13 +82,18 @@ impl SymbolTable {
         }
     }
 
-    pub fn create_scope(&mut self, name: Identifier) {
-        assert!(!self.does_child_scope_exist(&*name),
-                "Cannot create scope that already exists");
+    pub fn create_scope(&mut self, name: Identifier) -> errors::Result<()> {
+        if !self.does_child_scope_exist(&*name) {
+            return Err(errors::ErrorKind::ScopeAlreadyExists(name).into());
+        }
 
         let new_scope = self.scope_tree.add_node(HashMap::new());
-        self.scope_tree
-            .add_edge(self.current_scope.get(), new_scope, name);
+        self.scope_tree.add_edge(
+            self.current_scope.get(),
+            new_scope,
+            name,
+        );
+        Ok(())
     }
 
     pub fn does_child_scope_exist(&self, name: &str) -> bool {
@@ -72,16 +101,34 @@ impl SymbolTable {
             .is_some()
     }
 
-    pub fn enter_scope(&self, name: &str) {
-        let child_scope = self.find_child_scope(name, self.current_scope.get())
-            .expect("Cannot enter scope that does not exist");
-        self.current_scope.set(child_scope);
+    pub fn enter_scope(&self, name: &str) -> errors::Result<()> {
+        match self.find_child_scope(name, self.current_scope.get()) {
+            Some(child_scope) => {
+                self.current_scope.set(child_scope);
+                Ok(())
+            }
+            None => Err(
+                errors::ErrorKind::ScopeDoesNotExist(name.to_string()).into(),
+            ),
+        }
     }
 
-    pub fn exit_scope(&self) {
-        let parent_scope = self.get_parent_scope(self.current_scope.get())
-            .expect("Cannot exit out of global scope");
-        self.current_scope.set(parent_scope);
+    pub fn exit_scope(&self) -> errors::Result<()> {
+        match self.get_parent_scope(self.current_scope.get()) {
+            Some(parent_scope) => {
+                self.current_scope.set(parent_scope);
+                Ok(())
+            }
+            None => Err(errors::ErrorKind::CannotExitGlobalScope.into()),
+        }
+    }
+
+    pub fn insert_symbol(&mut self, name: Identifier, symbol: Symbol) -> errors::Result<()> {
+        let current_scope = self.current_scope.get();
+        let symbol_node = self.get_symbol_node_mut(current_scope);
+        symbol_node.insert(name, symbol).map(|symbol| ()).ok_or(
+            errors::ErrorKind::SymbolAlreadyExists(symbol).into(),
+        )
     }
 
     pub fn lookup_local_symbol(&self, name: &str) -> Option<&Symbol> {
@@ -105,12 +152,6 @@ impl SymbolTable {
         }
     }
 
-    pub fn insert_symbol(&mut self, name: Identifier, symbol: Symbol) -> Option<Symbol> {
-        let current_scope = self.current_scope.get();
-        let symbol_node = self.get_symbol_node_mut(current_scope);
-        symbol_node.insert(name, symbol)
-    }
-
     fn find_child_scope(&self, name: &str, scope: NodeIndex) -> Option<NodeIndex> {
         let edge = self.scope_tree
             .edges_directed(scope, Direction::Outgoing)
@@ -128,14 +169,14 @@ impl SymbolTable {
     }
 
     fn get_symbol_node(&self, scope: NodeIndex) -> &SymbolNode {
-        self.scope_tree
-            .node_weight(scope)
-            .expect("\"scope\" must reference an existing symbol node")
+        self.scope_tree.node_weight(scope).expect(
+            "\"scope\" must reference an existing symbol node",
+        )
     }
 
     fn get_symbol_node_mut(&mut self, scope: NodeIndex) -> &mut SymbolNode {
-        self.scope_tree
-            .node_weight_mut(scope)
-            .expect("\"scope\" must reference an existing symbol node")
+        self.scope_tree.node_weight_mut(scope).expect(
+            "\"scope\" must reference an existing symbol node",
+        )
     }
 }
