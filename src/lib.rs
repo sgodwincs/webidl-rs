5ba860cf7ed6ae5f76a3b867a44452e073ac1db4
Scott Godwin <sgodwincs@gmail.com>
2017-10-08 23:01:30 -0400

    remove unused dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,12 @@
-#![cfg_attr(feature="clippy", feature(plugin))]
-#![cfg_attr(feature="clippy", plugin(clippy))]
-#![deny(fat_ptr_transmutes,
-        missing_copy_implementations,
-        missing_debug_implementations,
-        missing_docs,
-        trivial_casts,
-        trivial_numeric_casts,
-        unsafe_code,
-        unused_import_braces,
-        unused_qualifications,
-        variant_size_differences)]
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+#![deny(fat_ptr_transmutes, missing_copy_implementations, missing_debug_implementations,
+       missing_docs, trivial_casts, trivial_numeric_casts, unsafe_code, unused_import_braces,
+       unused_qualifications, variant_size_differences)]
 
 //! Contains a lexer for lexing the WebIDL grammar.
 
-#[macro_use]
-extern crate error_chain;
 extern crate lalrpop_util;
-extern crate petgraph;
 
 /// Contains lexer related structures and functions for lexing the WebIDL grammar.
 mod lexer;
