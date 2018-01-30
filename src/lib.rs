50392c2960f2e8791b94ca6b3118d4050ae2bc72
Scott Godwin <sgodwincs@gmail.com>
2018-01-30 15:07:12 -0500

    update main module doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
-#![deny(missing_copy_implementations, missing_debug_implementations,
-       missing_docs, trivial_casts, trivial_numeric_casts, unsafe_code, unused_import_braces,
-       unused_qualifications, variant_size_differences)]
+#![deny(missing_copy_implementations, missing_debug_implementations, missing_docs, trivial_casts,
+        trivial_numeric_casts, unsafe_code, unused_import_braces, unused_qualifications,
+        variant_size_differences)]
 
-//! Contains a lexer for lexing the WebIDL grammar.
+//! Contains a lexer and parser for the WebIDL grammar.
 
 extern crate lalrpop_util;
 
