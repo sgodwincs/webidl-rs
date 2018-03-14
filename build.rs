d9695b25372438591eebf0ddd0d719a06359548f
Matt Brubeck <mbrubeck@limpet.net>
2018-03-14 13:01:36 -0700

    Place generated files in `$OUT_DIR`
    This prevents generated files from ending up in the source directory
    where they may be accidentally packaged or vendored.

--- build.rs
+++ build.rs
@@ -2,5 +2,8 @@ extern crate lalrpop;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/parser/grammar.lalrpop");
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new()
+        .use_cargo_dir_conventions()
+        .process()
+        .unwrap();
 }
